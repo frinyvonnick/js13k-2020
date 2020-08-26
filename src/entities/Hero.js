b9f6ac809769e25ee17b13d65be11e7383483a5d
Yvonnick FRIN <frin.yvonnick@gmail.com>
2020-08-26 20:19:24 +0200

    :lipstick: Add hero's animations (#4)
    - Idle
    - Run
    - Jump
    - Fall

--- src/entities/Hero.js
+++ src/entities/Hero.js
@@ -1,42 +1,101 @@
-import { Sprite, keyPressed, clamp } from "kontra";
+import { Sprite, SpriteSheet, keyPressed, clamp } from "kontra";
+import spritesheet from "../assets/spritesheet-heros.png";
 
 const MAX_SPEED = 10;
 
 export function makeHero() {
-  return Sprite({
-    x: 100, // starting x,y position of the sprite
-    y: 10,
-    color: "red", // fill color of the sprite rectangle
-    width: 200, // width and height of the sprite rectangle
-    height: 200,
-    isGrounded: false,
-    dx: 0, // move the sprite 2px to the right every frame
-    handleJump: function () {
-      if (this.isGrounded && keyPressed("space")) {
-        this.dy = -MAX_SPEED;
-      }
-      this.isGrounded = false;
-    },
-    handleMovement: function () {
-      this.dx = 0;
-      if (keyPressed("q")) {
-        this.dx = -2;
-      }
-      if (keyPressed("d")) {
-        this.dx = 2;
-      }
+  return new Promise(function (resolve, reject) {
+    const image = new Image();
+    image.src = spritesheet;
 
-      // Limit speed
-      this.dx = clamp(-MAX_SPEED, MAX_SPEED, this.dx);
-      this.dy = clamp(-MAX_SPEED, MAX_SPEED, this.dy);
-    },
+    image.onload = function () {
+      const spritesheet = SpriteSheet({
+        image,
+        frameWidth: 250 / 5,
+        frameHeight: 175 / 3,
+        animations: {
+          run: {
+            frames: [3, "5..7"],
+            frameRate: 10,
+          },
+          idle: {
+            frames: [8, "10..13"],
+            frameRate: 10,
+          },
+          jump: {
+            frames: [4, 9, 14],
+            frameRate: 10,
+          },
+          fall: {
+            frames: "0..2",
+            frameRate: 10,
+          },
+        },
+      });
 
-    update: function () {
-      this.advance();
-      this.ddy = 0.4;
+      const animationSprite = Sprite({
+        anchor: { x: 0.5, y: 0.5 },
+        width: 50, // width and height of the sprite rectangle
+        height: 175 / 3,
+        animations: spritesheet.animations,
+      })
 
-      this.handleJump();
-      this.handleMovement();
-    },
+      resolve(
+        Sprite({
+          anchor: { x: 0.5, y: 0.5 },
+          x: 100, // starting x,y position of the sprite
+          y: 10,
+          width: 20, // width and height of the sprite rectangle
+          height: 175 / 3,
+          isGrounded: false,
+          dx: 0, // move the sprite 2px to the right every frame
+          children: [animationSprite],
+          handleJump: function () {
+            if (this.isGrounded && keyPressed("space")) {
+              this.dy = -MAX_SPEED;
+            }
+            this.isGrounded = false;
+          },
+          handleMovement: function () {
+            this.dx = 0;
+            if (keyPressed("q")) {
+              this.dx = -2;
+            }
+            if (keyPressed("d")) {
+              this.dx = 2;
+            }
+
+            // Limit speed
+            this.dx = clamp(-MAX_SPEED, MAX_SPEED, this.dx);
+            this.dy = clamp(-MAX_SPEED, MAX_SPEED, this.dy);
+          },
+          update: function () {
+            this.advance();
+
+            this.ddy = 0.4;
+
+            this.handleJump();
+            this.handleMovement();
+
+            if (this.dx > 0) {
+              this.scaleX = 1
+            } else if (this.dx < 0) {
+              this.scaleX = -1
+            }
+
+            const animationSprite = this.children[0]
+            if (this.dx === 0 && this.dy === 0.4) {
+              animationSprite.playAnimation("idle");
+            } else if (this.dy === 0.4) {
+              animationSprite.playAnimation("run");
+            } else if (this.dy < 0.4) {
+              animationSprite.playAnimation("jump");
+            } else if (this.dy > 0.4) {
+              animationSprite.playAnimation("fall");
+            }
+          },
+        })
+      );
+    };
   });
 }
