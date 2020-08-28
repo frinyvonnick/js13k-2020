dee24165b8cdbd3fa19a91e3b8d51b4c84614e9f
FRIN Yvonnick <frin.yvonnick@gmail.com>
2020-08-28 23:56:56 +0200

    :sparkles: Save created sprites

--- utils/DesignScene.js
+++ utils/DesignScene.js
@@ -1,9 +1,18 @@
-import { Scene, Sprite, getPointer, onPointerDown, Vector } from "kontra";
+import {
+  Scene,
+  Sprite,
+  getPointer,
+  onPointerDown,
+  Vector,
+  keyPressed,
+} from "kontra";
 
 import * as Ground from "../src/entities/Ground";
 
 export function makeDesignScene() {
   return Promise.all([]).then(([]) => {
+    const entities = [];
+
     const sprite = Ground.makeEntity({
       ...Ground.defaultValues,
       anchor: { x: 0.5, y: 0.5 },
@@ -12,17 +21,25 @@ export function makeDesignScene() {
     const scene = Scene({
       id: "game",
       children: [sprite],
-      update: () => {
+      hasPressedSave: false,
+      update: function() {
         const pointer = getPointer();
         sprite.x = pointer.x;
         sprite.y = pointer.y;
+
+        if (keyPressed("s")) {
+          this.hasPressedSave = true;
+        } else if (this.hasPressedSave) {
+          this.hasPressedSave = false;
+          save(entities);
+        }
       },
     });
 
     onPointerDown(function (e, object) {
       const { x, y } = getPointer();
       scene.addChild(cloneSprite(sprite));
-      console.log("click", { x, y });
+      entities.push({ ...Ground.defaultValues, x, y });
     });
 
     return scene;
@@ -33,4 +50,13 @@ function cloneSprite(sprite) {
   const clonedSprite = new Sprite({ ...sprite });
   clonedSprite.position = Vector(sprite.position.x, sprite.position.y);
   return clonedSprite;
+}
+
+function save(entities) {
+  fetch("http://localhost:7000", {
+    method: "POST",
+    body: JSON.stringify(entities),
+  })
+    .then((res) => res.text())
+    .then(console.log);
 }
--- utils/design-client.js
+++ utils/design-client.js
@@ -5,6 +5,7 @@ import { isDesignMode } from "./env";
 
 const { canvas } = init();
 
+initKeys();
 initPointer();
 
 console.log("isDesignMode", isDesignMode);
@@ -12,14 +13,6 @@ console.log("isDesignMode", isDesignMode);
 console.log("load entities");
 fetch("http://localhost:7000")
   .then((res) => res.json())
-  .then(console.log);
-
-console.log("save entities");
-fetch("http://localhost:7000", {
-  method: "POST",
-  body: JSON.stringify([{ x: 0, y: 0 }]),
-})
-  .then((res) => res.text())
   .then(console.log);
 
 Promise.all([makeDesignScene()]).then(([designScene]) => {
