4e5d94692ec2d83fbe4fead5c3a88a243e50f2da
Yvonnick FRIN <frin.yvonnick@gmail.com>
2020-09-06 22:09:38 +0200

    :sparkles: Add slide platforms (#16)
    Co-authored-by: Vincent Poesmann <vincent.poesmann@gmail.com>

---
+++ src/entities/Slide.js
@@ -0,0 +1,22 @@
+import { Sprite } from "kontra";
+import { commonValues } from "./common";
+
+export const defaultValues = {
+  ...commonValues,
+  width: 50,
+  height: 50,
+  color: "blue",
+};
+
+export const makeEntity = function (props = defaultValues) {
+  return Sprite({
+    ...props,
+    type: "Slide",
+    render: function () {
+      this.context.beginPath();
+      this.context.fillStyle = this.color;
+      this.context.rect(0, -2, this.width, this.height);
+      this.context.fill();
+    },
+  });
+};
