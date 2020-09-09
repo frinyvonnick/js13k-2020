057bff17adb06b1e10d099cd632e8701fcfc2608
Yvonnick FRIN <frin.yvonnick@gmail.com>
2020-09-09 08:46:16 +0200

    :sparkles: Add credit screen and end conditions (#19)

---
+++ utils/entities/End.js
@@ -0,0 +1,30 @@
+import { Sprite } from "kontra";
+import { commonValues } from "./common";
+
+import { render as r } from "../../src/entities/Ground";
+
+export const defaultValues = {
+  ...commonValues,
+  width: 200,
+  height: 200,
+  color: "pink",
+};
+
+export function computeProps(props) {
+  return {
+    ...defaultValues,
+    ...props,
+    type: "End",
+  };
+}
+
+export const makeEntity = function (props) {
+  return Sprite({
+    ...computeProps(props),
+    render: r,
+  });
+};
+
+export const render = r;
+
+
